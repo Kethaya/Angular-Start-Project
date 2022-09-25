987d34e60fdfe39fec1b627b36c44640d2828a59
Kethaya <kavindumadusha8@gmail.com>
2022-09-25 11:21:10 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { ErrorPageComponent } from './Components/error-page/error-page.component';
+import { GalleryComponent } from './Components/gallery/gallery.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ContentComponent } from './Components/Layout/content/content.component';
+import { ServiceComponent } from './Components/service/service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AuthComponent},
+  {
+    path: 'home',
+     component: ContentComponent,
+     children: [
+      {path: '', component: HomeComponent},
+      {path: 'Service', component: ServiceComponent},
+      {path: 'Gallery', component: GalleryComponent},
+      {path: 'About', component: AboutComponent},
+      {path: 'Contact', component: ContactComponent}
+     ]
+  },
+  {path: 'not-found', component: ErrorPageComponent},
+  {path: '**', redirectTo: '/not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './Components/home/home.component';
+import { HeaderComponent } from './Components/Layout/header/header.component';
+import { ContentComponent } from './Components/Layout/content/content.component';
+import { FooterComponent } from './Components/Layout/footer/footer.component';
+import { ServiceComponent } from './Components/service/service.component';
+import { GalleryComponent } from './Components/gallery/gallery.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { ErrorPageComponent } from './Components/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    ServiceComponent,
+    GalleryComponent,
+    AboutComponent,
+    ContactComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
